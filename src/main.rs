7f474d37ca8fd566f02636f8d37340d5ce4e4c2b
IThundxr <me@ithundxr.dev>
2024-10-15 10:40:54 -0400

    Add libs

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,37 @@ use axum::Router;
 use axum::routing::get;
 use axum_github_webhook_extract::{GithubEvent, GithubToken};
 use serde::Deserialize;
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
-    dotenvy::dotenv().ok();
+    let level = if cfg!(debug_assertions) {
+        tracing::Level::DEBUG
+    } else {
+        tracing::Level::INFO
+    };
 
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_max_level(level)
+        .init();
+
+    dotenvy::dotenv().ok();
 
     let github_secret = env::var("GITHUB_SECRET")
         .expect("Missing GITHUB_SECRET Environment Variable");
 
     let app = Router::new()
         .route("/", get(root))
-        .with_state(GithubToken(Arc::new(github_secret)));
+        .with_state(GithubToken(Arc::new(github_secret)))
+        .layer(TraceLayer::new_for_http());
 
-    let addr = "0.0.0.0:3000";
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    println!("TcpListener bound to: {}", addr);
+    let ip = env::var("APP_IP").unwrap_or("0.0.0.0".to_string());
+    let port = env::var("APP_PORT").unwrap_or("3000".to_string());
+    let address = format!("{}:{}", ip, port);
+
+    log::info!("Listening on {}", address);
+
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
